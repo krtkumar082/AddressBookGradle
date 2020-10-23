94bdab1781bb7dea05f50151a2e9c98071c01c39
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-10-23 23:54:53 +0530

    [Kirti]Added:read and write using csv

--- src/main/java/com/addressbook/AddressBookIOService.java
+++ src/main/java/com/addressbook/AddressBookIOService.java
@@ -2,14 +2,24 @@ package com.addressbook;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+import com.opencsv.CSVWriter;
 
 public class AddressBookIOService {
-	public static String CONTACT_FILE_NAME= "personContactFile.txt";
-
+	public static String CONTACT_FILE_NAME= "ContactsFile.txt";
+	public static final String CONTACT_FILE_NAME_CSV = "./person.csv";
+	
 	public List<Contacts> readData() {
 		List<Contacts> contactsList = new ArrayList<>();
 		try {
@@ -46,4 +56,35 @@ public class AddressBookIOService {
 			e.printStackTrace();
 		}
 	}
+	
+	public List<Contacts> readCSVData() {
+		List<Contacts> contactsList = new ArrayList<>();
+		try {
+			Reader reader = Files.newBufferedReader(Paths.get(CONTACT_FILE_NAME_CSV));
+			CsvToBean<Contacts> csvToBean = new CsvToBeanBuilder<Contacts>(reader)
+					.withType(Contacts.class).withIgnoreLeadingWhiteSpace(true).build();
+
+			contactsList = csvToBean.parse();
+			reader.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return contactsList;
+	}
+
+	public boolean writeCSVData(List<Contacts> contactList) {
+		try (Writer writer = Files.newBufferedWriter(Paths.get(CONTACT_FILE_NAME_CSV))) {
+			StatefulBeanToCsv<Contacts> beanToCsv = new StatefulBeanToCsvBuilder<Contacts>(writer)
+					.withQuotechar(CSVWriter.NO_QUOTE_CHARACTER).build();
+
+			beanToCsv.write(contactList);
+		} catch (CsvRequiredFieldEmptyException | CsvDataTypeMismatchException | IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
 }
--- src/test/java/AddressBook/AddressBookIOServiceTest.java
+++ src/test/java/AddressBook/AddressBookIOServiceTest.java
@@ -14,9 +14,9 @@ public class AddressBookIOServiceTest {
 	public void writeContactsToFile() {
 		AddressBookIOService addressBookFileIOService = new AddressBookIOService();
 		List<Contacts> list = new ArrayList<>();
-		Contacts contact1 = new Contacts("Kirti", "Kumar", "btd", "btd", "btd", "331313", "91 9999999999", "abc@google.com");
-		Contacts contact2 = new Contacts("Test", "Test", "test", "test", "test", "987345", "91 8888888888", "abc@gmail.com");
-		Contacts contact3 = new Contacts("Test1", "Test", "test", "test", "test", "987345", "91 8888888888", "abc@gmail.com");
+		Contacts contact1 = new Contacts("Kirti", "Kumar", "sik", "sik", "raj", "331313", "91 9999999999", "abc@google.com");
+		Contacts contact2 = new Contacts("Deepak", "Kumar", "sik", "sik", "raj", "332315", "91 8888888888", "abc@gmail.com");
+		Contacts contact3 = new Contacts("Kk", "Fenin", "jap", "jap", "raj", "332316", "91 7777777777", "asdf@yahoo.com");
 		list.add(contact1);
 		list.add(contact2);
 		list.add(contact3);
